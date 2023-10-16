9ae67bb42233c489a5583ee109fa63ed373b1f02
kravchenski <max5045955@gmail.com>
2023-10-16 21:52:20 +0300

    feat: complete task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.maxRange = null;
+    this.minRange = null;
+    this.result = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.maxRange = max;
+    this.minRange = min;
+  }
 
-    }
+  guess() {
+    this.result = Math.ceil((this.minRange + this.maxRange) / 2);
+    return this.result;
+  }
 
-    guess() {
+  lower() {
+    this.maxRange = this.result;
+  }
 
-    }
+  greater() {   
+    this.minRange = this.result
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  }
 }
 
 module.exports = GuessingGame;
